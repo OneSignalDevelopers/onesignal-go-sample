a1970226a8af0f41af14ef146f83b6b6c29e9113
William Shepherd <will@codeincolor.io>
2022-09-15 21:50:10 +0900

    Abstract client detials

---
+++ helpers/client.go
@@ -0,0 +1,20 @@
+package onesignalHelpers
+
+import (
+	"context"
+	"onesignal"
+	"os"
+)
+
+func osAuthCtx() {
+	context.WithValue(
+		context.Background(),
+		onesignal.AppAuth,
+		os.Getenv("App_ID")
+	)
+}
+
+func osApiClient() {
+	configuration := onesignal.NewConfiguration()
+	apiClient := onesignal.NewAPIClient(configuration)
+}
