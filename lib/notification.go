d83e387cf7a30400f64ab011fb525e4961651be6
William Shepherd <williamrshepherd@gmail.com>
2023-08-02 13:23:45 -0500

    Extract client config

--- lib/notification.go
+++ lib/notification.go
@@ -8,18 +8,17 @@ import (
 	"github.com/OneSignal/onesignal-go-api/v2"
 )
 
-var configuration = onesignal.NewConfiguration()
-var apiClient = onesignal.NewAPIClient(configuration)
+var config = onesignal.NewConfiguration()
+var apiClient = onesignal.NewAPIClient(config)
+var appId = os.Getenv("APP_ID")
+var restApiKey = os.Getenv("REST_API_KEY")
+var authCtx = context.WithValue(
+	context.Background(),
+	onesignal.AppAuth,
+	restApiKey,
+)
 
 func PushNotification() {
-	appId := os.Getenv("APP_ID")
-	restApiKey := os.Getenv("REST_API_KEY")
-	osAuthCtx := context.WithValue(
-		context.Background(),
-		onesignal.AppAuth,
-		restApiKey,
-	)
-
 	notification := *onesignal.NewNotification(appId)
 	notification.SetIncludedSegments([]string{"Subscribed Users"})
 	notification.SetIsIos(false)
@@ -27,7 +26,7 @@ func PushNotification() {
 	stringMap := onesignal.StringMap{En: &message}
 	notification.Contents = *onesignal.NewNullableStringMap(&stringMap)
 
-	resp, r, err := apiClient.DefaultApi.CreateNotification(osAuthCtx).Notification(notification).Execute()
+	resp, r, err := apiClient.DefaultApi.CreateNotification(authCtx).Notification(notification).Execute()
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error when calling `CreateNotification`: %v\n", err)
